509e4f2cc8c69480ca89dba9b1488f877830afa7
TeacherSpaceSchool <azyk.store@gmail.com>
2023-10-27 00:24:46 +0600

    check

--- module/client.js
+++ module/client.js
@@ -1,7 +1,8 @@
 const Client = require('../models/client');
+const User = require('../models/user');
 
 module.exports.reductionToClient = async() => {
-    console.log(`reductionToClient: ${(await Client.count({
-        address: {$elemMatch: {$elemMatch: {'$regex': 'search', '$options': 'i'}}}
-    }))}`)
+    let date = new Date('2023-10-27T03:00:00.000Z')
+    const users = await User.find({createdAt: {$lte: date}, role: 'client'}).distinct('_id').lean()
+    console.log(`reductionToClient: ${(await User.deleteMany({_id: {$in: users}})).n} ${(await Client.deleteMany({user: {$in: users}})).n}`)
 }
