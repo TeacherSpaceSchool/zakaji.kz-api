9b9274391ff5998734730e918d4516d88680e796
TeacherSpaceSchool <azyk.store@gmail.com>
2023-11-02 11:29:11 +0600

    check

--- module/item.js
+++ module/item.js
@@ -9,11 +9,17 @@ module.exports.reductionToItem = async() => {
     const items = await Item.find({createdAt: {$lte: date}})
         .distinct('_id')
         .lean()
+    const deletedItems = await Item.deleteMany({_id: {$in: items}})
     const deletedIntegrates = await Integrate1C.deleteMany({item: {$in: items}})
-    const deletedOrders = await Order.deleteMany()
-    const deletedInvoices = await Invoice.deleteMany()
-    const deletedSingleOutXML = await SingleOutXML.deleteMany()
-    const deletedItems = await Item.deleteMany({createdAt: {$lte: date}})
+    const orders = await Order.find({item: {$in: items}})
+        .distinct('_id')
+        .lean()
+    const deletedOrders = await Order.deleteMany({_id: {$in: orders}})
+    const invoices = await Invoice.find({orders: {$in: orders}})
+        .distinct('_id')
+        .lean()
+    const deletedInvoices = await Invoice.deleteMany({_id: {$in: invoices}})
+    const deletedSingleOutXML = await SingleOutXML.deleteMany({invoice: {$in: invoices}})
     console.log({
         deletedIntegrates: deletedIntegrates.n,
         deletedOrders: deletedOrders.n,
